9ba241f3e82374359ba3fa0aab44efa434eab6fd
Artyom Gritsuk <postagric@gmail.com>
2020-09-18 23:54:32 +0300

    Add CMYK class

---
+++ ColorHelper/Color/CMYK.cs
@@ -0,0 +1,18 @@
+﻿namespace ColorHelper.Color
+{
+    public class CMYK
+    {
+        private byte C { get; set; }
+        private byte M { get; set; }
+        private byte Y { get; set; }
+        private byte K { get; set; }
+
+        public CMYK(byte c, byte m, byte y, byte k)
+        {
+            this.C = c;
+            this.M = m;
+            this.Y = y;
+            this.K = k;
+        }
+    }
+}
