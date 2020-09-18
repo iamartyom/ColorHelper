6c4a23ecd37ccc637befd84f303f5d222c7c3ee2
Artyom Gritsuk <postagric@gmail.com>
2020-09-18 09:30:42 +0300

    Add RGB class

---
+++ ColorHelper/Color/RGB.cs
@@ -0,0 +1,16 @@
+﻿namespace ColorHelper.Color
+{
+    public class RGB
+    {
+        private byte R { get; set; }
+        private byte G { get; set; }
+        private byte B { get; set; }
+
+        public RGB(byte r, byte g, byte b)
+        {
+            this.R = r;
+            this.G = g;
+            this.B = b;
+        }
+    }
+}
