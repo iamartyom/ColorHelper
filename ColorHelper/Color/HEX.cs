df8baa518bc4396b7bf1914add8b17c3711ec7bd
Artyom Gritsuk <postagric@gmail.com>
2020-09-20 10:58:21 +0300

    Improve HEX class

--- ColorHelper/Color/HEX.cs
+++ ColorHelper/Color/HEX.cs
@@ -2,11 +2,22 @@
 {
     public class HEX
     {
-        public string Value { get; set; }
+        private string _value;
+
+        public string Value
+        {
+            get { return _value; }
+            set { _value = (value.IndexOf('#') == 0) ? value.Substring(1) : value; }
+        }
 
         public HEX(string value)
         {
             this.Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.Value == (obj as HEX)?.Value;
         }
 
         public override string ToString()
