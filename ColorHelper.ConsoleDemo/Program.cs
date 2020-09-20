cba955be48ecaaf842f028d2d3358df44ca08f7f
Artyom Gritsuk <postagric@gmail.com>
2020-09-20 06:53:12 +0300

    Use RgbToCmyk method in ConsoleDemo project

--- ColorHelper.ConsoleDemo/Program.cs
+++ ColorHelper.ConsoleDemo/Program.cs
@@ -8,6 +8,9 @@ namespace ColorHelper.ConsoleDemo
     {
         static void Main(string[] args)
         {
+            CMYK cmyk = ColorConverter.RgbToCmyk(new RGB(100, 200, 60));
+            Console.WriteLine(cmyk);
+
             HEX hex = ColorConverter.RgbToHex(new RGB(100, 100, 100));
             Console.WriteLine(hex);
 
