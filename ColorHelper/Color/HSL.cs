d927b5323e27c754a031acc2f306aab6945397d7
Artyom Gritsuk <postagric@gmail.com>
2020-10-07 23:10:46 +0300

    Add HSL class

---
+++ ColorHelper/Color/HSL.cs
@@ -0,0 +1,32 @@
+﻿namespace ColorHelper
+{
+    public class HSL : IColor
+    {
+        public byte H { get; set; }
+        public byte S { get; set; }
+        public byte L { get; set; }
+
+        public HSL(byte h, byte s, byte l)
+        {
+            this.H = h;
+            this.S = s;
+            this.L = l;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var result = (HSL)obj;
+
+            return (
+                result != null &&
+                this.H == result.H &&
+                this.S == result.S &&
+                this.L == result.L);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.H}° {this.S}% {this.L}%";
+        }
+    }
+}
